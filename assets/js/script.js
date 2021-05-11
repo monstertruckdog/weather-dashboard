2adf3f39355c709c92e87d6917bb6c80e72f2a47
Bryan Civils <hermanbciv@gmail.com>
2021-05-11 00:36:26 -0400

    Initiate build of '5-day forecast section; experiment with new API call endpoints

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,10 +65,8 @@ function getLonLat() {
 // UV INDEX
 
 function getCurrentWeather(longitude, latitude) {
-    // RENOVATION getLonLat();
     if (longitude && latitude) {
-        console.log(`LONGITUDE / LATITUDE (inside getCurrentWeather:  ${longitude} / ${latitude}}`);
-        var currentWeatherValuesArray = [];
+        console.log(`LONGITUDE / LATITUDE (inside getCurrentWeather:  ${longitude} / ${latitude}`);
         var requestCurrentCity =  'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&exclude=hourly,minutely,daily,alerts&appid=' + key + '&units=imperial'
     
         fetch(requestCurrentCity)
@@ -78,43 +76,17 @@ function getCurrentWeather(longitude, latitude) {
                     console.log(`response call was OK`);
                     response.json().then(function (data) {
                         console.log(data);
-                        currentWeatherValuesArray.push(data.current.temp);
-                        currentWeatherValuesArray.push(data.current.wind_speed);
-                        currentWeatherValuesArray.push(data.current.humidity);
-                        currentWeatherValuesArray.push(data.current.uvi);
-
                         console.log(`LONGITUDE:  ${longitude}`);
                         console.log(`LATITUDE:  ${latitude}`);
-                        console.log(`CURRENT WEATHER VALUES:  ${currentWeatherValuesArray}`)
 
                         var currentWeatherValuesList = $('<ul>');
                         currentWeatherValuesList.attr('class', 'current-weather-values-ul');
                         currentWeatherContainer.append(currentWeatherValuesList);
-                        // RENOVATION currentWeatherValuesList.append('<li class="temp-li">' + currentWeatherValuesArray[0] + '°F</li>');
-                        // RENOVATION currentWeatherValuesList.append('<li class="wind-li">' + currentWeatherValuesArray[1] + 'MPH</li>');
-                        // RENOVATION currentWeatherValuesList.append('<li class="humidity-li">' + currentWeatherValuesArray[2] + '%</li>');
-                        // RENOVATION currentWeatherValuesList.append('<li class="uvi-li"><span class="uvi-li-span>' + currentWeatherValuesArray[3] + '</span></li>');
                         currentWeatherValuesList.append('<li class="current-li temp-li">TEMPERATURE:  ' + data.current.temp + '°F</li>');
                         currentWeatherValuesList.append('<li class="current-li wind-li">WIND SPEED:  ' + data.current.wind_speed + 'MPH</li>');
                         currentWeatherValuesList.append('<li class="current-li humidity-li">HUMIDITY:  ' + data.current.humidity + '%</li>');
                         currentWeatherValuesList.append('<li class="current-li uvi-li">UV INDEX:  <span class="uvi-li-span">' + data.current.uvi + '</span></li>');
-                        /*
-                        $( document ).ready(function() {
-                            if (data.current.uvi >= 0 && data.current.uvi < 3) {
-                                console.log(`INSIDE SPAN`)
-                                var uVIndex = $('.uvi-li-span');
-                                uVIndex.attr('id', 'uvi-minimal');
-                            } else if (data.current.uvi >= 3 && data.current.uvi < 5) {
-                                uVIndex.attr('id', 'uvi-low');
-                            } else if (data.current.uvi >= 5 && data.current.uvi < 7) {
-                                uVIndex.attr('id', 'uvi-moderate');
-                            } else if (data.current.uvi >= 7 && data.current.uvi < 10) {
-                                uVIndex.attr('id', 'uvi-high');
-                            } else if (data.current.uvi >= 10) {
-                                uVIndex.attr('id', 'uvi-very-high');
-                            };
-                        });
-                        */
+
                         if (data.current.uvi >= 0 && data.current.uvi < 3) {
                             console.log(`INSIDE SPAN`)
                             var uVIndex = $('.uvi-li-span');
@@ -128,34 +100,39 @@ function getCurrentWeather(longitude, latitude) {
                         } else if (data.current.uvi >= 10) {
                             uVIndex.attr('id', 'uvi-very-high');
                         };
-                });
-            } else {
-                console.log(`AN ERROR HAS OCCURRED IN THE GET METHOD TO THE API.  PLEASE REVIEW AND TRY AGAIN`)
-                return;
-            }
-        });
-    /* RENOVATION
-    var currentWeatherValuesList = $('<ul>');
-    currentWeatherValuesList.attr('class', 'current-weather-values-ul');
-    currentWeatherContainer.append(currentWeatherValuesList);
-    currentWeatherValuesList.append('<li class="temp-li">' + currentWeatherValuesArray[0] + '°F</li>');
-    currentWeatherValuesList.append('<li class="wind-li">' + currentWeatherValuesArray[1] + 'MPH</li>');
-    currentWeatherValuesList.append('<li class="humidity-li">' + currentWeatherValuesArray[2] + '%</li>');
-    currentWeatherValuesList.append('<li class="uvi-li"><span class="uvi-li-span>' + currentWeatherValuesArray[3] + '</span></li>');
-    */
-    // Used UV Index category definitions from National Weather Service Climate Prediction Center:
-    // URL:  https://www.cpc.ncep.noaa.gov/products/stratosphere/uv_index/uv_info.shtml
-    /* RENOVATION
-    if (currentWeatherValuesArray[3] >= 0 && currentWeatherValuesArray[3] < 3) {
-        $('.uvi-li-span').setAttr('id', 'uvi-minimal');
-    } else if (currentWeatherValuesArray[3] >= 3 && currentWeatherValuesArray[3] < 5) {
-        $('.uvi-li-span').setAttr('id', 'uvi-low');
-    } else if (currentWeatherValuesArray[3] >= 5 && currentWeatherValuesArray[3] < 7) {
-        $('.uvi-li-span').setAttr('id', 'uvi-moderate');
-    } else if (currentWeatherValuesArray[3] >= 7 && currentWeatherValuesArray[3] < 10) {
-        $('.uvi-li-span').setAttr('id', 'uvi-high');
-    } else if (currentWeatherValuesArray[3] >= 10) {
-        $('.uvi-li-span').setAttr('id', 'uvi-very-high');
-    */
+
+                        var fiveDayDataGet = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + key + '&units=imperial'
+
+                        fetch(fiveDayDataGet)
+                            .then(function (response) {
+                                console.log(response.status);
+                                if (response.ok) {
+                                    console.log(`response call was OK`);
+                                    response.json().then(function (data) {
+                                        console.log(data);
+                                        console.log(`LONGITUDE:  ${longitude}`);
+                                        console.log(`LATITUDE:  ${latitude}`);
+                                        console.log(`LENGTH OF OBJECT:  ${data.list.length}`)
+                                        
+                                        $('.fiveday-container').append('<h4 id="fiveday-title">5-Day Forecast:</h4>');
+                                        $('.fiveday-container').append('<div class="fiveday-card-container"></div>');
+                                        $('.fiveday-card-container').append('<div class="fiveday-card" id="day-1">CARD 1</div>')
+
+                                        var dayData = []
+                                        for (var i = 0; i < 5; i += 8) {
+                                            dayData.push(data.list[i])
+                                            console.log(`ITERATION ${i}:  ${dayData}`);
+                                        }
+                                    
+                                    })
+                                }
+                            });
+                    });
+                } else {
+                    console.log(`AN ERROR HAS OCCURRED IN THE GET METHOD TO THE API.  PLEASE REVIEW AND TRY AGAIN`);
+                    return;
+                };
+
+            });
     };
 };
