737346b33595cdb06cd463ce3e17c40a72e49c0b
Bryan Civils <hermanbciv@gmail.com>
2021-07-22 01:30:28 -0400

    Clean comments and console.logs; update README

--- README.md
+++ README.md
@@ -3,13 +3,21 @@
 
 ## Description
 
-In order to both demonstrate and improve the knowledge and skills required for working with third party APIs, students are tasked with creating a weather dashboard using the [OpenWeather](https://openweathermap.org/) API portfolio alongside HTML, CSS and JavaScript.
+In order to both demonstrate and improve the knowledge and skills required for working with third party APIs, students are tasked with creating a weather forecasting dashboard using the [OpenWeather](https://openweathermap.org/) API portfolio alongside HTML, CSS and JavaScript.
+
+While working to satisfy the project requirements, work was also done to improve skills in:
+
+* writing JavaScript with as little JQuery as possible
+* styling using [Materialize CSS](https://materializecss.com/)
+* making and streamlining third-party API calls using callbacks
+* creating a useful and functional modal
+* writing regular expressions and performing text string manipulations
 
 ## Requirements
 
 A user must be able to access the page and enter the name of a city.  The application must do the following in response:
 
-* Return valid weather data for the entered location (using an OpenWeather API)
+* Return valid, current weather data for the entered location (using an OpenWeather API)
 * Display valid weather data for the entered location for:
 
 	- Current day (ie *date the user executes the city submission*)
@@ -17,6 +25,57 @@ A user must be able to access the page and enter the name of a city.  The applic
 	- List of cities that were previously forecasted (*stored in and recalled from local storage*)
 		- Each of the city names listed must be clickable to execute a new forecast call
 
+## How to use
+
+1. [Access page hosted on GitPages](https://monstertruckdog.github.io/weather-dashboard/)
+2. Enter name of city in "Search for a City" input
+3. Click "SUBMIT" button
+
+## Usage
+
+#### Simple search with results
+
+![01_simple_with-irregular-characters](./assets/readme_assets/01_simple_with-irregular-characters.png)
+
+#### Simple search with local storage displayed
+
+![02_simple_with-local-storage](./assets/readme_assets/02_simple_with-local-storage.png)
+
+#### Search with invalid city name error
+
+![03_invalid](./assets/readme_assets/03_invalid.png)
+
+
+## Highlights and Features
+
+* Simple, single page design
+* Appearence and styling enhanced with [Materialize CSS](https://materializecss.com/)
+* OpenWeather API's used:
+    - ["Current weather data"](https://openweathermap.org/current)
+	- ["One Call API"](https://openweathermap.org/api/one-call-api)
+* Weather information is added, changed, removed dynamically on the 
+* Display of city name is normalized
+* City names with whitespace, hyphen and other characters are handled appropriately to allow for search
+* Invalid city names are rejected with immediate user feedback messaging
+* Failed API calls return common language messages
+* Local storage items are checked for duplication before adding new entries
+* Previously searched cities are checked for duplication before display
+
 ## Future Enhancements
 
-* Implement API key 
+* Allow for "enter"/"return" key to submit user entered city search value
+* Continue to reduce reliance on JQuery methods (*personal challenge*)
+* Reduce number of global variables defined
+* Include loading spinner icon and animation
+* Expand user search options:
+  - Allow/handle entry of zip code
+  - Allow/handle entry of city and state
+  - Allow/handle entry of city, state and country
+* Allow user to select between Imperial/US units (°F) and SI Derived units (°C) displayed in results
+* Further improvements to handling of city name that does not exist
+* Implement API key cloaking (with server-less JS)
+
+## Application Access
+
+* **GitHub Repository**:  [monstertruckdog/weather-dashboard](https://github.com/monstertruckdog/weather-dashboard)
+* **Deployed (*GitHub Pages*)**:  [weather-dashboard](https://monstertruckdog.github.io/weather-dashboard/)
--- assets/js/script.js
+++ assets/js/script.js
@@ -24,11 +24,7 @@ function storeCity() {
     if (/[^a-zA-Z\s-]/.test(cityName)) {
         currentWeatherContainer.text('');
     } else {
-        console.log(`--> CONTENTS OF cityStore:  ${cityStore}`)
-        console.log(`--> INDEX OF cityStore CONTENTS:  ${cityStore.indexOf(cityName)}`)
-        console.log(`--> indexOf(cityName):  ${cityStore.indexOf(cityName) === -1}`);
         cityStore.forEach((city) => city.toLowerCase());
-        console.log(`--> CONTENTS OF cityStore (after forEach):  `, cityStore);
         if (cityStore.indexOf(cityName) === -1) {
             cityStore.push(cityName.toLowerCase());
             localStorage.setItem('CITY', JSON.stringify(cityStore));
@@ -40,12 +36,9 @@ function storeCity() {
 }
 
 function displayNewSearchedCity() {
-    console.log(`VALUE OF cityName:  ${cityName}`);
-    console.log(`VALUE OF cityStore AT INDEX cityStore.length:  ${cityStore[cityStore.length - 1]}`)
     if (cityName === cityStore[cityStore.length - 1]) {
         var searchListItem = document.createElement('li');
         var searchBtn = document.createElement('button');
-        // searchBtn.setAttribute(`id`, `search-city-${storedSearches}`);
         searchBtn.setAttribute(`class`, `stored-search-btn`);
         searchBtn.setAttribute(`onclick`, `goSavedCity()`)
         searchBtn.className += ` btn blue-grey darken-2`
@@ -58,15 +51,10 @@ function displayNewSearchedCity() {
 }
 
 function displayAllSearchedCities() {
-    // $('.recent-search-list').innerHTML = "";
     document.getElementsByClassName("recent-search-list").innerHTML = "";
     var storedSearches = '';
     var storedSearches = JSON.parse(localStorage.getItem('CITY'))
-    console.log(`--> storedSearches CONTENT:  ${storedSearches}`)
     if (storedSearches) {
-        console.log(`--> storedSearches:  ${storedSearches}`)
-        console.log(`--> storedSearches Length:  ${storedSearches.length}`)
-    
         for (var i = 0; i < storedSearches.length; i++) {
             var searchListItem = document.createElement('li');
             var searchBtn = document.createElement('button');
@@ -85,90 +73,13 @@ function displayAllSearchedCities() {
 
 function cityNameValidator(name) {
     if (/[^a-zA-Z\s-]/.test(name)) {
-        console.log(`invalid city name`)
         eWarningText.append(`City name is invalid`);
     }
 }
 
-function firstLetterUpperCase(name) { // <-- THIS FUNCTION IS DEPRECATED; keeping in case it's needed before a more final version is completed
-    let firstLetter;
-    function convertCase() {
-        firstLetter = name.match(/\b[a-zA-Z]/g)
-        console.log(`--> firstLetterUpperCase - firstLetter FOUND:  `, firstLetter)
-        console.log(`--> firstLetterUpperCase - word value:  `, name);
-        firstLetterUpper = firstLetter[0].toUpperCase();
-        return firstLetterUpper;
-    }
-    console.log(`--> firstLetterUpperCase - 102 - name VALUE:  `, name)
-    if (name.match(/\b[a-zA-Z]/g).length === 1) {
-        // singleWordFirstLetter = word.match(/\b[a-zA-Z]/g);
-        // console.log(`--> firstLetterUpperCase - singleWordFirstLetter:  `, singleWordFirstLetter);
-        // console.log(`--> firstLetterUpperCase - firstLetter UpperCased:  `, firstLetter[0].toUpperCase());
-        // word.replace(/\b[a-zA-Z]/g, firstLetter[0].toUpperCase())
-        // convertCase(firstLetter);
-        name.replace(/\b[a-zA-Z]/g, convertCase())
-        console.log(`--> firstLetterUpperCase - firstLetterUpper:  `, firstLetterUpper)
-        console.log(`--> firstLetterUpperCase - word after replace:  `, name);
-    } else if (firstLetter && firstLetter.length > 1) {
-        console.log(`--> firstLetterUpperCase:  `, 'to be determined')
-    }
-    // const wordUpper = word.replace(/\b[a-zA-Z]/g, /\b[a-zA-Z]/g.toUpperCase());
-    // console.log(`--> wordUpper:  `, wordUpper);
-    return name;
-}
-
-function cityNameFormatter(name) { // <-- THIS FUNCTION IS DEPRECATED; keeping around in case needed before a more final version is completed
-    cityNameValidator(name);
-    console.log(`--> 'name' VALUE:  `, name)
-    nameSplit = name.split('');
-    if (nameSplit.includes(' ') === true) {
-        console.log('City name contains a space character (\' \')');
-        // firstLetter = nameSplit.shift().toUpperCase();
-        // nameSplit.unshift(firstLetter);
-        // console.log(`--> nameSplit (after unshift):  `, nameSplit)
-        /*
-        for (let i = 0; i < nameSplit.length; i++) {
-            if (nameSplit[i] === ' ') {
-                firstLetterNextWord = nameSplit[i + 1]
-                firstLetterNextWord.toUpperCase();
-                nameSplit.splice(nameSplit[i + 1], 1, firstLetterNextWord)
-                nameCleaned = nameSplit.join('');
-                storeCity(cityName);
-                currentWeather(nameCleaned);
-                getLonLat();
-            }
-            
-        }
-        */
-        firstLetterUpperCase(name);
-        //nameCleaned = nameSplit.join('');
-    } else {
-        /*
-        console.log(`--> 'name' VALUE at [0]:  `, nameSplit[0]);
-        console.log(`--> nameSplit (00) (before shift declared):  `,  nameSplit);
-        firstLetter = nameSplit.shift().toUpperCase();
-        nameSplit.unshift(firstLetter);
-        nameCleaned = nameSplit.join('');
-        console.log(`--> nameCleaned:  `,  nameCleaned);
-        */
-       firstLetterUpperCase(name)
-       storeCity(cityName);
-       currentWeather();
-       getLonLat();
-    }
-}
-
 function currentWeather() {
-    console.log(`--> currentWeather Test Results on cityName:  `, /[^a-zA-Z\s-]/.test(cityName));
     if (/[^a-zA-Z\s-]/.test(cityName)) {
-        
-        // currentWeatherContainer.attr('id', 'current-container-dynamic');
-        // currentWeatherContainer.innerHTML = "";
-        // eAllWeatherContainer.innerHTML = "";
-        // currentWeatherContainer.text('');
-        // document.querySelector(".container-master-weather").innerHTML = "" // <-- this works
         document.getElementById('current-container-border').setAttribute('id', 'current-container-border-hide')
-        // document.getElementsByClassName('current-weather-container').innerHTML = "";
         document.getElementById('five-container-border').setAttribute('id', 'five-container-border-hide')
         document.getElementById('five-container-border-hide').innerHTML = "";
         return;
@@ -184,9 +95,6 @@ function currentWeather() {
 function goSavedCity() {
     event.preventDefault();
     cityName = $(event.target).text();
-    console.log(`--> VALUE FOR cityName (from goSavedCity):  `, $(event.target).text())
-    console.log(`--> cityName (variable value - from goSavedCity):  ${cityName}`);
-    console.log(`--> cityName (expression value - from goSavedCity):  ${$(event.target).text()}`);
     currentWeather(cityName);
     getLonLat();
 };
@@ -199,7 +107,6 @@ function getLonLat() {
     var requestCurrentCity = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + key
     fetch(requestCurrentCity)
         .then(function (response) {
-            console.log(response.status);
             if (response.ok) {
                 response.json().then(function (data) {
                     longitude = data.coord.lon
@@ -207,19 +114,6 @@ function getLonLat() {
                     getCurrentWeather(longitude, latitude);
                     return { longitude, latitude }
                 });
-            /*} else if (response.status === 404) {
-                response.json().then(function (data) {
-                    longitude = null
-                    latitude = null
-                    console.log(`City name \"${cityName}\" included in search was not found`)
-                    document.querySelector(".container-master-weather").innerHTML = ''
-                    console.log(`allWeatherContainer:  `, eAllWeatherContainer)
-                    while (eWarningText.hasChildNodes()) {
-                        eWarningText.removeChild(eWarningText.firstChild);
-                    }
-                    eWarningText.append(`City name is not found`)
-                    return { longitude, latitude }
-                });*/
             } else {
                 console.log(`AN ERROR HAS OCCURRED IN THE GET METHOD TO THE API.  PLEASE REVIEW AND TRY AGAIN`)
                 return;
@@ -228,12 +122,10 @@ function getLonLat() {
 };
 
 function getCurrentWeather(longitude, latitude) {
-    console.log(`--> getCurrentWeather - longitude, latitude value:  `, longitude + ', ', latitude);
     if (longitude && latitude) {
         var requestCurrentCity =  'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&exclude=hourly,minutely,daily,alerts&appid=' + key + '&units=imperial'
         fetch(requestCurrentCity)
             .then(function (response) {
-                console.log(response.status);
                 if (response.ok) {
                     response.json().then(function (data) {
                         var currentWeatherValuesList = $('<ul>');
@@ -259,8 +151,7 @@ function getCurrentWeather(longitude, latitude) {
 
                     });
                 } else if (response.status === 404) {
-                    // document.getElementById('all-weather-container').innerHTML('');
-                    // document.getElementById('fiveday-container-dynamic').innerHTML('');
+                    // TO DO:  this is not necessary
                     return
                 } else {
                     console.log(`AN ERROR HAS OCCURRED IN THE GET METHOD TO THE API.  PLEASE REVIEW AND TRY AGAIN (getCurrentWeather)`);
@@ -277,7 +168,6 @@ function getFiveDayWeather(longitude, latitude) {
 
         fetch(fiveDayDataGet)
             .then(function (response) {
-                console.log(response.status);
                 if (response.ok) {
                     $('.fiveday-container').html('');
                     fivedayWeatherContainer.attr('id', 'five-container-border');
@@ -304,8 +194,7 @@ function getFiveDayWeather(longitude, latitude) {
                     
                     })
                 } else if (response.status === 404) {
-                    console.log(`--> fiveDayDataGet returned:  ${response.status}`)
-                    console.log(`--> Clearing fiveDay data from HTML`)
+                    // TO DO:  this is likely not necessary
                     $('.fiveday-container').html('');
                     return;
                 };
@@ -318,31 +207,22 @@ function getFiveDayWeather(longitude, latitude) {
 };
 
 function getCityValue() {
+    // this clears the invalid city name error message text
     while (eWarningText.hasChildNodes()) {
         eWarningText.removeChild(eWarningText.lastChild);
     }
     cityName = $('#city-entry').val().trim().toLowerCase();
-    console.log(`cityName SUBMITTED:  ${cityName}`);
-    // cityNameFormatter(cityName);
-    // storeCity(cityName);
-    // currentWeather(nameCleaned);
-    // getLonLat();
     cityNameValidator(cityName);
     storeCity(cityName);
     currentWeather();
     getLonLat();
 };
 
-/*
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, options);
-});
-*/
+
+// Materialize JS to initialize the "About" modal
 $(document).ready(function(){
     $('.modal').modal();
   });
 
-// document.querySelector('#nav-about').addEventListener('click', about());
-
+// Displays existing "Recent Seraches" buttons when accessing page
 init();
