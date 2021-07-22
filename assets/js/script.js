12e22d80d6ab2721b15db1df44c13cd69d10aad7
Bryan Civils <hermanbciv@gmail.com>
2021-07-22 00:31:46 -0400

    Major updates to data entry logic and other JS elements; added modal and styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,7 @@ var eWarnings = document.getElementById('warning');
 var eWarningText = document.getElementById('warning-text');
 var cityEntry = $('#city-entry');
 var citySubmitBtn = $('#submit-btn')
+var eAllWeatherContainer = $('.all-weather-container');
 var fivedayWeatherContainer = $('.fiveday-container');
 
 var currentDate = moment().format('MM/DD/YYYY');
@@ -20,20 +21,22 @@ var cityStore = [];
 var key = `e8a298c777dff96133e6b579f54b4339`
 
 function storeCity() {
-    console.log(`--> CONTENTS OF cityStore:  ${cityStore}`)
-    console.log(`--> INDEX OF cityStore CONTENTS:  ${cityStore.indexOf(cityName)}`)
-    console.log(`--> indexOf(cityName):  ${cityStore.indexOf(cityName) === -1}`);
-    cityStore.forEach((city) => city.toLowerCase());
-    console.log(`--> CONTENTS OF cityStore (after forEach):  `, cityStore);
-    if (cityStore.indexOf(cityName) === -1) {
-        cityStore.push(cityName.toLowerCase());
-        localStorage.setItem('CITY', JSON.stringify(cityStore));
-        displayNewSearchedCity();
-        
+    if (/[^a-zA-Z\s-]/.test(cityName)) {
+        currentWeatherContainer.text('');
     } else {
-        return;
+        console.log(`--> CONTENTS OF cityStore:  ${cityStore}`)
+        console.log(`--> INDEX OF cityStore CONTENTS:  ${cityStore.indexOf(cityName)}`)
+        console.log(`--> indexOf(cityName):  ${cityStore.indexOf(cityName) === -1}`);
+        cityStore.forEach((city) => city.toLowerCase());
+        console.log(`--> CONTENTS OF cityStore (after forEach):  `, cityStore);
+        if (cityStore.indexOf(cityName) === -1) {
+            cityStore.push(cityName.toLowerCase());
+            localStorage.setItem('CITY', JSON.stringify(cityStore));
+            displayNewSearchedCity();
+        } else {
+            return;
+        }
     }
-    // displaySearchedCities();
 }
 
 function displayNewSearchedCity() {
@@ -87,15 +90,43 @@ function cityNameValidator(name) {
     }
 }
 
-function cityNameFormatter(name) {
+function firstLetterUpperCase(name) { // <-- THIS FUNCTION IS DEPRECATED; keeping in case it's needed before a more final version is completed
+    let firstLetter;
+    function convertCase() {
+        firstLetter = name.match(/\b[a-zA-Z]/g)
+        console.log(`--> firstLetterUpperCase - firstLetter FOUND:  `, firstLetter)
+        console.log(`--> firstLetterUpperCase - word value:  `, name);
+        firstLetterUpper = firstLetter[0].toUpperCase();
+        return firstLetterUpper;
+    }
+    console.log(`--> firstLetterUpperCase - 102 - name VALUE:  `, name)
+    if (name.match(/\b[a-zA-Z]/g).length === 1) {
+        // singleWordFirstLetter = word.match(/\b[a-zA-Z]/g);
+        // console.log(`--> firstLetterUpperCase - singleWordFirstLetter:  `, singleWordFirstLetter);
+        // console.log(`--> firstLetterUpperCase - firstLetter UpperCased:  `, firstLetter[0].toUpperCase());
+        // word.replace(/\b[a-zA-Z]/g, firstLetter[0].toUpperCase())
+        // convertCase(firstLetter);
+        name.replace(/\b[a-zA-Z]/g, convertCase())
+        console.log(`--> firstLetterUpperCase - firstLetterUpper:  `, firstLetterUpper)
+        console.log(`--> firstLetterUpperCase - word after replace:  `, name);
+    } else if (firstLetter && firstLetter.length > 1) {
+        console.log(`--> firstLetterUpperCase:  `, 'to be determined')
+    }
+    // const wordUpper = word.replace(/\b[a-zA-Z]/g, /\b[a-zA-Z]/g.toUpperCase());
+    // console.log(`--> wordUpper:  `, wordUpper);
+    return name;
+}
+
+function cityNameFormatter(name) { // <-- THIS FUNCTION IS DEPRECATED; keeping around in case needed before a more final version is completed
     cityNameValidator(name);
     console.log(`--> 'name' VALUE:  `, name)
     nameSplit = name.split('');
     if (nameSplit.includes(' ') === true) {
         console.log('City name contains a space character (\' \')');
-        firstLetter = nameSplit.shift().toUpperCase();
-        nameSplit.unshift(firstLetter);
-        console.log(`--> nameSplit (after unshift):  `, nameSplit)
+        // firstLetter = nameSplit.shift().toUpperCase();
+        // nameSplit.unshift(firstLetter);
+        // console.log(`--> nameSplit (after unshift):  `, nameSplit)
+        /*
         for (let i = 0; i < nameSplit.length; i++) {
             if (nameSplit[i] === ' ') {
                 firstLetterNextWord = nameSplit[i + 1]
@@ -108,66 +139,87 @@ function cityNameFormatter(name) {
             }
             
         }
-        nameCleaned = nameSplit.join('');
+        */
+        firstLetterUpperCase(name);
+        //nameCleaned = nameSplit.join('');
     } else {
+        /*
         console.log(`--> 'name' VALUE at [0]:  `, nameSplit[0]);
         console.log(`--> nameSplit (00) (before shift declared):  `,  nameSplit);
         firstLetter = nameSplit.shift().toUpperCase();
         nameSplit.unshift(firstLetter);
         nameCleaned = nameSplit.join('');
         console.log(`--> nameCleaned:  `,  nameCleaned);
-        storeCity(cityName);
-        currentWeather(nameCleaned);
-        getLonLat();
+        */
+       firstLetterUpperCase(name)
+       storeCity(cityName);
+       currentWeather();
+       getLonLat();
     }
 }
 
-function currentWeather(nameCleaned) {
-    if (/[^a-zA-Z\s-]/.test(nameCleaned)) {
-        currentWeatherContainer.attr('id', 'current-container-dynamic');
-        currentWeatherContainer.text('');
+function currentWeather() {
+    console.log(`--> currentWeather Test Results on cityName:  `, /[^a-zA-Z\s-]/.test(cityName));
+    if (/[^a-zA-Z\s-]/.test(cityName)) {
+        
+        // currentWeatherContainer.attr('id', 'current-container-dynamic');
+        // currentWeatherContainer.innerHTML = "";
+        // eAllWeatherContainer.innerHTML = "";
+        // currentWeatherContainer.text('');
+        // document.querySelector(".container-master-weather").innerHTML = "" // <-- this works
+        document.getElementById('current-container-border').setAttribute('id', 'current-container-border-hide')
+        // document.getElementsByClassName('current-weather-container').innerHTML = "";
+        document.getElementById('five-container-border').setAttribute('id', 'five-container-border-hide')
+        document.getElementById('five-container-border-hide').innerHTML = "";
+        return;
     } else {
-        currentWeatherContainer.attr('id', 'current-container-dynamic');
+        currentWeatherContainer.attr('id', 'current-container-border');
         currentWeatherContainer.text('');
-        currentWeatherHeadCity.text(nameCleaned);
+        currentWeatherHeadCity.text(cityName);
         currentWeatherHeadDate.text(currentDate);
-        console.log(`from getCurrentWeather - CITY NAME:  ${cityName}`);
-        console.log(`from getCurrentWeather - CURRENT DATE:  ${currentDate}`);
-        currentWeatherContainer.append('<h4 id="city-name">' + nameCleaned + ' <span id="current-date">(' + currentDate + ')</span></h4>');
+        currentWeatherContainer.append('<h4 id="city-name">' + cityName + ' <span id="current-date">(' + currentDate + ')</span></h4>');
     };
 };
-
-
 
 function goSavedCity() {
     event.preventDefault();
     cityName = $(event.target).text();
+    console.log(`--> VALUE FOR cityName (from goSavedCity):  `, $(event.target).text())
     console.log(`--> cityName (variable value - from goSavedCity):  ${cityName}`);
     console.log(`--> cityName (expression value - from goSavedCity):  ${$(event.target).text()}`);
-    currentWeather();
+    currentWeather(cityName);
     getLonLat();
-}
+};
 
 function init() {
     displayAllSearchedCities();
-}
+};
+
 function getLonLat() {
     var requestCurrentCity = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + key
-    
     fetch(requestCurrentCity)
         .then(function (response) {
             console.log(response.status);
             if (response.ok) {
-                console.log(`response call was OK (getLonLat)`);
                 response.json().then(function (data) {
-                    console.log(data);
                     longitude = data.coord.lon
                     latitude = data.coord.lat
                     getCurrentWeather(longitude, latitude);
-                    console.log(`LONGITUDE:  ${longitude}`);
-                    console.log(`LATITUDE:  ${latitude}`);
                     return { longitude, latitude }
                 });
+            /*} else if (response.status === 404) {
+                response.json().then(function (data) {
+                    longitude = null
+                    latitude = null
+                    console.log(`City name \"${cityName}\" included in search was not found`)
+                    document.querySelector(".container-master-weather").innerHTML = ''
+                    console.log(`allWeatherContainer:  `, eAllWeatherContainer)
+                    while (eWarningText.hasChildNodes()) {
+                        eWarningText.removeChild(eWarningText.firstChild);
+                    }
+                    eWarningText.append(`City name is not found`)
+                    return { longitude, latitude }
+                });*/
             } else {
                 console.log(`AN ERROR HAS OCCURRED IN THE GET METHOD TO THE API.  PLEASE REVIEW AND TRY AGAIN`)
                 return;
@@ -176,20 +228,14 @@ function getLonLat() {
 };
 
 function getCurrentWeather(longitude, latitude) {
+    console.log(`--> getCurrentWeather - longitude, latitude value:  `, longitude + ', ', latitude);
     if (longitude && latitude) {
-        console.log(`LONGITUDE / LATITUDE (inside getCurrentWeather:  ${longitude} / ${latitude}`);
         var requestCurrentCity =  'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&exclude=hourly,minutely,daily,alerts&appid=' + key + '&units=imperial'
-    
         fetch(requestCurrentCity)
             .then(function (response) {
                 console.log(response.status);
                 if (response.ok) {
-                    console.log(`response call was OK (getCurrentWeather)`);
                     response.json().then(function (data) {
-                        console.log(data);
-                        console.log(`LONGITUDE:  ${longitude}`);
-                        console.log(`LATITUDE:  ${latitude}`);
-
                         var currentWeatherValuesList = $('<ul>');
                         currentWeatherValuesList.attr('class', 'current-weather-values-ul');
                         currentWeatherContainer.append(currentWeatherValuesList);
@@ -199,8 +245,6 @@ function getCurrentWeather(longitude, latitude) {
                         currentWeatherValuesList.append('<li class="current-li uvi-li">UV INDEX:  <span class="uvi-li-span">' + data.current.uvi + '</span></li>');
 
                         if (data.current.uvi >= 0 && data.current.uvi < 3) {
-                            console.log(`INSIDE SPAN`)
-                            var uVIndex = $('.uvi-li-span');
                             $('.uvi-li-span').attr('id', 'uvi-minimal');
                         } else if (data.current.uvi >= 3 && data.current.uvi < 5) {
                             $('.uvi-li-span').attr('id', 'uvi-low');
@@ -214,6 +258,10 @@ function getCurrentWeather(longitude, latitude) {
                         getFiveDayWeather(longitude, latitude);
 
                     });
+                } else if (response.status === 404) {
+                    // document.getElementById('all-weather-container').innerHTML('');
+                    // document.getElementById('fiveday-container-dynamic').innerHTML('');
+                    return
                 } else {
                     console.log(`AN ERROR HAS OCCURRED IN THE GET METHOD TO THE API.  PLEASE REVIEW AND TRY AGAIN (getCurrentWeather)`);
                     return;
@@ -231,24 +279,11 @@ function getFiveDayWeather(longitude, latitude) {
             .then(function (response) {
                 console.log(response.status);
                 if (response.ok) {
-                    console.log(`response call was OK`);
                     $('.fiveday-container').html('');
-                    // searchBtn.className += ` btn blue-grey darken-2 ${storedSearches[i]}`
-                    // document.getElementsByClassName('fiveday-container').setAttribute('id', 'fiveday-container-dynamic');
-                    
-                    // document.getElementsByClassName("fiveday-container").innerHTML = "";
-                    fivedayWeatherContainer.attr('id', 'fiveday-container-dynamic');
+                    fivedayWeatherContainer.attr('id', 'five-container-border');
                     response.json().then(function (data) {
-                        console.log('DATA', data);
-                        
-                        console.log(`LONGITUDE:  ${longitude}`);
-                        console.log(`LATITUDE:  ${latitude}`);
-                        // document.getElementsByClassName("fiveday-container").id = `fiveday-container-dynamic`;
                         $('.fiveday-container').append('<h4 id="fiveday-title">5-Day Forecast:</h4>');
-
-                        var fiveDayData = []
                         $('.fiveday-container').append('<div class="fiveday-card-container"></div>');
-                        
                         for (var q = 1; q < 6; q++) {
                             var dayData = [];
                             dayData = [ moment.unix(data.daily[q].dt).format("MM/DD/YYYY"),
@@ -258,10 +293,6 @@ function getFiveDayWeather(longitude, latitude) {
                                         data.daily[q].humidity
                                       ];
                             // ICON EXAMPLE:  http://openweathermap.org/img/wn/10d@2x.png
-                            console.log(`--> WEATHER ICON CODE (From array):  ${dayData[1]}`);
-                            console.log(`--> WEATHER DESCRIPTION: ${data.daily[q].weather.description}`)
-                            console.log(`--> WEATHER ICON CODE (raw loop extract - A):  ${data.daily[q].weather.icon}`)
-                            console.log(`--> WEATHER ICON CODE (raw loop extract - B):  ${data.daily[q].weather[0].icon}`)
                             $('.fiveday-card-container').append(`<div class="fiveday-card-${q}" id="fdc-card"></div>`);
                             $(`.fiveday-card-${q}`).append(`<ul class="five-day-weather-values-list-${q}">`);
                             $(`.five-day-weather-values-list-${q}`).append(`<li class="fiveday-card-li-${q}" id="date">${dayData[0]}</li>`)
@@ -269,12 +300,15 @@ function getFiveDayWeather(longitude, latitude) {
                             $(`.five-day-weather-values-list-${q}`).append(`<li class="fiveday-card-li-${q}" id="temp"><strong>TEMPERATURE</strong>:  ${dayData[2]} °F</li>`)
                             $(`.five-day-weather-values-list-${q}`).append(`<li class="fiveday-card-li-${q}" id="windspeed"><strong>WIND SPEED</strong>:  ${dayData[3]} MPH</li>`)
                             $(`.five-day-weather-values-list-${q}`).append(`<li class="fiveday-card-li-${q}" id="humidity"><strong>HUMIDITY</strong>:  ${dayData[4]}%</li>`)
-                            console.log(`ITERATION B ${q}:  ${dayData}`);
-                            console.log(`ITERATION B[0] ${q}:  ${dayData[0]}`);
                         }
                     
                     })
-                }
+                } else if (response.status === 404) {
+                    console.log(`--> fiveDayDataGet returned:  ${response.status}`)
+                    console.log(`--> Clearing fiveDay data from HTML`)
+                    $('.fiveday-container').html('');
+                    return;
+                };
             });
     } else {
         console.log(`AN ERROR HAS OCCURRED IN THE GET METHOD TO THE API.  PLEASE REVIEW AND TRY AGAIN (getFiveDayWeather)`);
@@ -285,14 +319,30 @@ function getFiveDayWeather(longitude, latitude) {
 
 function getCityValue() {
     while (eWarningText.hasChildNodes()) {
-        eWarningText.removeChild(node.firstChild);
+        eWarningText.removeChild(eWarningText.lastChild);
     }
     cityName = $('#city-entry').val().trim().toLowerCase();
     console.log(`cityName SUBMITTED:  ${cityName}`);
-    cityNameFormatter(cityName);
+    // cityNameFormatter(cityName);
     // storeCity(cityName);
     // currentWeather(nameCleaned);
     // getLonLat();
+    cityNameValidator(cityName);
+    storeCity(cityName);
+    currentWeather();
+    getLonLat();
 };
+
+/*
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
+});
+*/
+$(document).ready(function(){
+    $('.modal').modal();
+  });
+
+// document.querySelector('#nav-about').addEventListener('click', about());
 
 init();
